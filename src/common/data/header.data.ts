f6001e4fa29f48b5a0812319aa0670f36aa68f11
8023time <2162105974@qq.com>
2025-11-17 22:53:13 +0800

    🚀 feat: 添加新的静态数据

--- src/common/data/header.data.ts
+++ src/common/data/header.data.ts
@@ -1,15 +1,17 @@
 /**
- * @description: 主导航配置
- * @version: 1.0.0
+ * @description: 网站footer配置
  */
 export const MAIN_NAV_CONFIG: {
   name: string;
-  href?: string;
+  to?: string;
   logo?: string | HTMLElement;
 }[] = [
-  { name: '首页', href: '/' },
-  { name: '技能', href: '/skill' },
-  { name: '数据', href: '/test' },
-  // { name: "网站合集", href: "/site" },
-  // { name: "网站分析", href: "/statistics" },
+  { name: '首页', to: '/' },
+  { name: '文章', to: '/docs' },
+  { name: '关于', to: '/test' },
+  { name: '友链', to: '/docs' },
+  { name: '技能', to: '/skill' },
+  { name: '网站合集', to: '/site' },
+  { name: '留言区', to: '/Comments' },
+  { name: '网站分析', to: '/statistics' },
 ];
