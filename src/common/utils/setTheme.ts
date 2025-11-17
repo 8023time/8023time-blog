091533989444e72db322a43557422bd5155bfd3a
8023time <2162105974@qq.com>
2025-11-17 22:54:58 +0800

    🚀 feat: 添加新的工具函数

--- src/common/utils/setTheme.ts
+++
@@ -1,9 +0,0 @@
-export function setTheme(theme: string) {
-  document.documentElement.setAttribute('data-theme', theme);
-}
-
-export function toggleTheme(currentTheme: string) {
-  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-  setTheme(newTheme);
-  return newTheme;
-}
