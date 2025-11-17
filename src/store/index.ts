a8cf31665afc16d296ec6bcd636ffa742606b811
8023time <2162105974@qq.com>
2025-11-17 22:41:51 +0800

    🚀 feat: 田间切换主题的 store 仓库

---
+++ src/store/index.ts
@@ -0,0 +1,3 @@
+export { default as useThemeStore } from './modules/theme';
+export type { ThemeType } from './modules/theme';
+export { setThemeToLocalStorage, applyThemeToDocument, setupSystemThemeListener } from './modules/theme';
