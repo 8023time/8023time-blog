d227cad4d6f8efafd4e066dd184963bb35d9bf8d
8023time <2162105974@qq.com>
2025-11-17 22:41:02 +0800

    🐞 fix: 修改旧的布局，添加新的布局，模仿TailwindCss样式

---
+++ src/layout/LineLayout/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export interface LineLayoutProps {
+  children: React.ReactNode;
+}
+
+const LineLayout: React.FC<LineLayoutProps> = ({ children }) => {
+  return (
+    <div className='isolate bg-gray-50 dark:bg-gray-950'>
+      <div className='max-w-screen overflow-x-hidden'>
+        {/* 左右侧线条背景（重复渐变+边框） */}
+        <div className='grid min-h-dvh grid-cols-1 grid-rows-[1fr_1px_auto_1px_auto] justify-center pt-14.25 [--gutter-width:2.5rem] lg:grid-cols-[var(--gutter-width)_minmax(0,var(--breakpoint-2xl))_var(--gutter-width)]'>
+          {/* 左侧线条 */}
+          <div className='col-start-1 row-span-full row-start-1 hidden border-x border-x-(--pattern-fg) bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed [--pattern-fg:var(--color-black)]/5 lg:block dark:[--pattern-fg:var(--color-white)]/10'></div>
+
+          {/* 中间内容区（包裹 Header + 页面内容 + Footer） */}
+          <div className='text-gray-950 lg:col-start-2 dark:text-white'>{children}</div>
+
+          {/* 右侧线条 */}
+          <div className='row-span-full row-start-1 hidden border-x border-x-(--pattern-fg) bg-[image:repeating-linear-gradient(315deg,_var(--pattern-fg)_0,_var(--pattern-fg)_1px,_transparent_0,_transparent_50%)] bg-[size:10px_10px] bg-fixed [--pattern-fg:var(--color-black)]/5 lg:col-start-3 lg:block dark:[--pattern-fg:var(--color-white)]/10'></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LineLayout;
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,21 +1,25 @@
 import Header from './Header';
 import Footer from './Footer';
-import Content from './Content';
+import LineLayout from './LineLayout';
 import { Outlet } from 'react-router';
 
 const Layout = () => {
   return (
-    <div className='flex min-h-screen flex-col justify-between'>
-      <Header />
-      <Content>
-        <main className='flex-1'>
-          <Outlet />
-        </main>
-      </Content>
-      <Footer />
-    </div>
+    <>
+      <LineLayout>
+        <Header />
+
+        <div className='flex flex-col'>
+          <div className='flex-1'>
+            <Outlet />
+          </div>
+        </div>
+
+        <Footer />
+      </LineLayout>
+    </>
   );
 };
 
 export default Layout;
-export { Header, Footer, Content };
+export { Header, Footer, LineLayout };
