283a9bcd13c13a4eeb22f3563f0f7d7d9fda06d0
8023time <2162105974@qq.com>
2025-11-19 20:22:50 +0800

    🚀 feat: 新增群居 顶部进度条 功能

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import './App.css';
 import type React from 'react';
 import router from '@router/index';
 import { RouterProvider } from 'react-router';
+import { useScroll, motion } from 'motion/react';
 import { FloatButton, ThemeInitializer } from '@components/index';
 
 const App: React.FC = () => {
+  const { scrollYProgress } = useScroll();
+
   return (
     <>
+      {/* 顶部进度条 */}
+      <motion.div className='fixed top-0 right-0 left-0 z-50 h-1 bg-blue-400' style={{ scaleX: scrollYProgress }} />
       {/* 上拉按钮 */}
       <FloatButton.BackTop />
       {/* 监听主题切换 */}
